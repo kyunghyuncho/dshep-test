56ca951783159ce0d0be567100185ed7572abd87
kyunghyun cho <kyunghyun.cho@nyu.edu>
2017-05-08 16:56:21 -0500

    latest

---
+++ env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PYTHONPATH=$PYTHONPATH:/home/kcho/.local/lib/python2.7/site-packages/
+export PYTHONPATH=$PYTHONPATH:/home/kcho/Tutorial/DLKit
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
